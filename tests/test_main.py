47da426e405e860559df25363896e5c4b298024c
Przemysław Hausman <przemyslaw.hausman@canonical.com>
2020-11-15 20:06:13 +0100

    Use pytest-click plugin

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,12 @@
 import pytest
 
-from click.testing import CliRunner
-
 import messages
 import main
 
 
 @pytest.mark.parametrize("option", ["-V", "--version"])
-def test_version(option):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, [option])
+def test_version(cli_runner, option):
+    result = cli_runner.invoke(main.cli, [option])
     assert result.exit_code == 0
     assert result.output == main.VERSION + "\n"
 
@@ -20,27 +17,24 @@ def test_version(option):
 
 
 @pytest.mark.parametrize("option", ["-h", "--help"])
-def test_cli_help(option):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, [option])
+def test_cli_help(cli_runner, option):
+    result = cli_runner.invoke(main.cli, [option])
     assert result.exit_code == 0
     # Strip all whitespace before comparing strings because click rewraps test
     assert "".join(messages.MAIN_HELP.split()) in "".join(result.output.split())
 
 
 @pytest.mark.parametrize("option", ["-h", "--help"])
-def test_power_help(option):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["power", option])
+def test_power_help(cli_runner, option):
+    result = cli_runner.invoke(main.cli, ["power", option])
     assert result.exit_code == 0
     # Strip all whitespace before comparing strings because click rewraps test
     assert "".join(messages.POWER_LONG_HELP.split()) in "".join(result.output.split())
 
 
 @pytest.mark.parametrize("option", ["-h", "--help"])
-def test_power_on_help(option):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["power", "on", option])
+def test_power_on_help(cli_runner, option):
+    result = cli_runner.invoke(main.cli, ["power", "on", option])
     assert result.exit_code == 0
     # Strip all whitespace before comparing strings because click rewraps test
     assert "".join(messages.POWER_ON_ACTION_LONG_HELP.split()) in "".join(
@@ -49,9 +43,8 @@ def test_power_on_help(option):
 
 
 @pytest.mark.parametrize("option", ["-h", "--help"])
-def test_power_off_help(option):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["power", "off", option])
+def test_power_off_help(cli_runner, option):
+    result = cli_runner.invoke(main.cli, ["power", "off", option])
     assert result.exit_code == 0
     # Strip all whitespace before comparing strings because click rewraps test
     assert "".join(messages.POWER_OFF_ACTION_LONG_HELP.split()) in "".join(
@@ -60,9 +53,8 @@ def test_power_off_help(option):
 
 
 @pytest.mark.parametrize("option", ["-h", "--help"])
-def test_power_stat_help(option):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["power", "stat", option])
+def test_power_stat_help(cli_runner, option):
+    result = cli_runner.invoke(main.cli, ["power", "stat", option])
     assert result.exit_code == 0
     # Strip all whitespace before comparing strings because click rewraps test
     assert "".join(messages.POWER_STAT_ACTION_LONG_HELP.split()) in "".join(
@@ -71,18 +63,16 @@ def test_power_stat_help(option):
 
 
 @pytest.mark.parametrize("option", ["-h", "--help"])
-def test_bootdev_help(option):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["bootdev", option])
+def test_bootdev_help(cli_runner, option):
+    result = cli_runner.invoke(main.cli, ["bootdev", option])
     assert result.exit_code == 0
     # Strip all whitespace before comparing strings because click rewraps test
     assert "".join(messages.BOOTDEV_LONG_HELP.split()) in "".join(result.output.split())
 
 
 @pytest.mark.parametrize("option", ["-h", "--help"])
-def test_bootdev_bios_help(option):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["bootdev", "bios", option])
+def test_bootdev_bios_help(cli_runner, option):
+    result = cli_runner.invoke(main.cli, ["bootdev", "bios", option])
     assert result.exit_code == 0
     # Strip all whitespace before comparing strings because click rewraps test
     assert "".join(messages.BOOTDEV_BIOS_ACTION_LONG_HELP.split()) in "".join(
@@ -91,9 +81,8 @@ def test_bootdev_bios_help(option):
 
 
 @pytest.mark.parametrize("option", ["-h", "--help"])
-def test_bootdev_disk_help(option):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["bootdev", "disk", option])
+def test_bootdev_disk_help(cli_runner, option):
+    result = cli_runner.invoke(main.cli, ["bootdev", "disk", option])
     assert result.exit_code == 0
     # Strip all whitespace before comparing strings because click rewraps test
     assert "".join(messages.BOOTDEV_DISK_ACTION_LONG_HELP.split()) in "".join(
@@ -102,9 +91,8 @@ def test_bootdev_disk_help(option):
 
 
 @pytest.mark.parametrize("option", ["-h", "--help"])
-def test_bootdev_pxe_help(option):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["bootdev", "pxe", option])
+def test_bootdev_pxe_help(cli_runner, option):
+    result = cli_runner.invoke(main.cli, ["bootdev", "pxe", option])
     assert result.exit_code == 0
     # Strip all whitespace before comparing strings because click rewraps test
     assert "".join(messages.BOOTDEV_PXE_ACTION_LONG_HELP.split()) in "".join(
@@ -117,6 +105,7 @@ def test_bootdev_pxe_help(option):
 #
 
 # SUCCESSFUL
+# - no machines provided
 # - single machine with short name
 # - explicit (without globs) multiple machines
 # - glob machine
@@ -127,46 +116,33 @@ def test_bootdev_pxe_help(option):
 # - ambiguous machine name
 
 
-def test_main(script_runner):
-    ret = script_runner.run("fce-ipmi", "--version")
-    assert ret.success
-    assert ret.stdout == main.VERSION + "\n"
-    assert ret.stderr == ""
-
-
-def test_power_stat_implicit_failed():
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["power"])
+def test_power_stat_implicit_failed(cli_runner):
+    result = cli_runner.invoke(main.cli, ["power"])
     assert result.exit_code == 1
 
 
 @pytest.mark.parametrize("actions", ["on", "off", "cycle", "stat"])
 @pytest.mark.parametrize("machines", ["compute-1"])
-def test_power_actions_failed(actions, machines):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["power", actions, machines])
+def test_power_actions_failed(cli_runner, actions, machines):
+    result = cli_runner.invoke(main.cli, ["power", actions, machines])
     assert result.exit_code == 1
 
 
 @pytest.mark.parametrize("actions", ["bios", "disk", "pxe"])
 @pytest.mark.parametrize("machines", ["compute-1"])
-def test_bootdev_actions_failed(actions, machines):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["bootdev", actions, machines])
+def test_bootdev_actions_failed(cli_runner, actions, machines):
+    result = cli_runner.invoke(main.cli, ["bootdev", actions, machines])
     assert result.exit_code == 1
 
 
 @pytest.mark.parametrize("machines", ["compute-1"])
-def test_console_failed(machines):
-    runner = CliRunner()
-    result = runner.invoke(main.cli, ["console", machines])
+def test_console_failed(cli_runner, machines):
+    result = cli_runner.invoke(main.cli, ["console", machines])
     assert result.exit_code == 1
 
 
-def test_power_stat_dry_run():
-
-    runner = CliRunner()
-    result = runner.invoke(
+def test_power_stat_dry_run(cli_runner):
+    result = cli_runner.invoke(
         main.cli,
         [
             "-s",
